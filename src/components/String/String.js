09cb6946973ba5a47ba4df85ac673fcc2892a11d
Shane Drury <shane.r.drury@gmail.com>
2015-04-09 09:44:27 +0100

    Moving to two canvases

--- src/components/String/String.js
+++ src/components/String/String.js
@@ -11,7 +11,6 @@ var String = React.createClass({
     },
 
     getInitialState: function() {
-
         return {
             overlay: null,
             stage: null
@@ -32,10 +31,8 @@ var String = React.createClass({
     renderCanvas: function() {
         var _this = this;
         var stage = this.state.stage;
-        var img = new Image();
         var layer = new Kinetic.Layer();
-        var ctx = layer.getCanvas().getContext('2d');
-        ctx.clearRect(0, 0, 200, 200);
+        var img = new Image();
         img.src = 'tile.png';
         img.onload = function() {
             var tile = new Kinetic.Image({
@@ -45,19 +42,37 @@ var String = React.createClass({
                 width: 100,
                 height: 100
             });
-
+            var rect = new Kinetic.Rect({
+                x: 0, y:0,
+                width: _this.props.width,
+                height: _this.props.height,
+                fill: 'red'
+            });
+            layer.on('mouseover', _this.click);
+            layer.add(rect);
             layer.add(tile);
+
+            // Need to make a fake canvas and draw from it to the real one
+            var ctx = _this.getContext(layer);
+            var imageData = ctx.getImageData(0, 0, 100, 100);
+            var overlay = _this.state.overlay;
+            for (var idx=0; idx < imageData.data.length; idx+=4) {
+                if (idx % 4 != 3) {
+                    if(overlay[idx+3] != 0){
+                        imageData.data[idx] = overlay[idx];
+                        imageData.data[idx+1] = overlay[idx+1];
+                        imageData.data[idx+2] = overlay[idx+2];
+                        imageData.data[idx+3] = overlay[idx+3];
+                    }
+                }
+            }
+            stage.add(layer);
+            ctx.putImageData(imageData, 0, 0);
         };
-        var rect = new Kinetic.Rect({
-            x: 0, y:0,
-            width: _this.props.width,
-            height: _this.props.height,
-            fill: 'red'
-        });
-        layer.on('mouseover', _this.click);
-        layer.add(rect);
-        stage.add(layer);
-        ctx.putImageData(_this.state.overlay, 0, 0);
+    },
+
+    getContext: function(layer) {
+        return layer.getCanvas().getContext('2d');
     },
 
     initStage: function() {
@@ -70,13 +85,13 @@ var String = React.createClass({
     },
 
     initOverlay: function() {
-        var overlay = new ImageData(100, 100);
+        var overlay = [];
         for (var idx=0; idx < 100 * 100 * 4; idx+=4) {
             if (idx%4 != 3){
-                overlay.data[idx] = 0;
-                overlay.data[idx+1] = 0;
-                overlay.data[idx+2] = 0;
-                overlay.data[idx+3] = 255;
+                overlay[idx] = 0;
+                overlay[idx+1] = 0;
+                overlay[idx+2] = 0;
+                overlay[idx+3] = 255;
             }
         }
         this.setState({overlay: overlay});
@@ -85,8 +100,8 @@ var String = React.createClass({
     revealOverlay: function(x, y) {
         var overlay = this.state.overlay;
         var idx = 4 * (x + y * 100);
-        overlay.data[idx+1] = 255; // TODO: Change this to alpha
-        //overlay.data[idx+3] = 0;
+        //overlay[idx+1] = 255; // TODO: Change this to alpha
+        overlay[idx+3] = 0;
         this.setState({overlay: overlay})
     },
 
